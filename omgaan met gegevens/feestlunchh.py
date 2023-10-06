81d4d7f0eaafbd84e1879311365b0cd84f14099e
B078 <sbverschoor7@gmail.com>
2023-10-06 10:57:08 +0200

    opdracht 1

--- omgaan met gegevens/feestlunchh.py
+++ omgaan met gegevens/feestlunchh.py
@@ -7,13 +7,15 @@ prijs_stokbrood = 2.78
 aantal_croissant = 17
 aantal_stokbroden = 2
 
+kortings_bonnen = 3
+waarde_bonnen = 0.50 
+
 #berekening
 
 prijzen_croissant = aantal_croissant * prijs_croissant
 prijzen_stokbroden = aantal_stokbroden * prijs_stokbrood
+aantal_korting = kortings_bonnen * waarde_bonnen
 
-totaal = prijzen_croissant + prijzen_stokbroden
+totaal = prijzen_croissant + prijzen_stokbroden - aantal_korting
 
-#resultaat
-print("Het te betalen bedrag is $",round(totaal, 2 ))
-      
+print(f"Uw te betalen bedrag is, {round(totaal, 2)} ")
