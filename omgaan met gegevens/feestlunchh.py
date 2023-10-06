81a971892a8642cfa68ec96adb267f32ef5cab4f
B078 <sbverschoor7@gmail.com>
2023-10-06 12:14:21 +0200

    VERBETER

--- omgaan met gegevens/feestlunchh.py
+++ omgaan met gegevens/feestlunchh.py
@@ -1,25 +1,25 @@
 
 #prijs per stuk
-prijs_croissant = 0.39
-prijs_stokbrood = 2.78
+PRIJS_CROISSANT = 0.39
+PRIJS_STOKBROOD = 2.78
 
 #aantallen
 aantal_croissant = 17
 aantal_stokbroden = 2
 
-kortings_bonnen = 3
-waarde_bonnen = 0.50 
+KORTINGS_BONNEN = 3
+WAARDE_BONNEN = 0.50 
 
 #berekening
 
-prijzen_croissant = aantal_croissant * prijs_croissant
-prijzen_stokbroden = aantal_stokbroden * prijs_stokbrood
+prijzen_croissant = aantal_croissant * PRIJS_CROISSANT
+prijzen_stokbroden = aantal_stokbroden * PRIJS_STOKBROOD
 
 totaal_kosten_zonder_korting = prijzen_croissant + prijzen_stokbroden
-aantal_korting = kortings_bonnen * waarde_bonnen
+aantal_korting = KORTINGS_BONNEN * WAARDE_BONNEN
 
 totaal_met_korting = prijzen_croissant + prijzen_stokbroden - aantal_korting
 
 #resultaat
 print(" ")
-print(f"De feestlunch kost je bij de bakker {round(totaal_met_korting, 2 )} euro voor de {aantal_croissant} croissantjes en de {aantal_stokbroden} stokbroden als de {kortings_bonnen} kortingsbonnen nog geldig zijn!")
+print(f"De feestlunch kost je bij de bakker {round(totaal_met_korting, 2 )} euro voor de {aantal_croissant} croissantjes en de {aantal_stokbroden} stokbroden als de {KORTINGS_BONNEN} kortingsbonnen nog geldig zijn!")
