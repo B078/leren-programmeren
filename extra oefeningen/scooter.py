c6934620760406cca68d82ee14368b2ea8d4d806
B078 <sbverschoor7@gmail.com>
2023-10-10 00:04:04 +0200

    verplaatsing

--- extra oefeningen/scooter.py
+++ extra oefeningen/scooter.py
@@ -1,15 +1,4 @@
-#Je scooter mankeert van alles. En kan een opknapbeurt gebruiken:
-# 2 nieuwe banden 2 nieuwe lampjes
-
-#de 2 banden zijn versleten
-#de 2 lampjes moeten vervangen
-#Kan je dat betalen?
-#Een nieuwe band kost: € 57,79
-#Arbeid per band kost: € 15,00
-#Een nieuw lampje kost: € 1,45
-#Lampjes vervangen per lampje kost: €2,10
-
-#waardes
+#waardes 
 arbeid_kosten_per_band = 15.00
 arbeid_lampje_vervangen_per_lamp = 2.10
 
@@ -18,16 +7,3 @@ nieuwe_lampje = 1.45
 
 aantal_banden = 2 
 aantal_lampjes = 2 
-
-#berekening
-bedrag_per_band = nieuwe_band + arbeid_kosten_per_band
-bedrag_per_lamp = nieuwe_lampje + arbeid_lampje_vervangen_per_lamp
-
-kosten_banden = bedrag_per_band * aantal_banden
-kosten_lampen = bedrag_per_lamp * aantal_lampjes
-
-totaal =  kosten_banden + kosten_lampen
-
-#output
-print(f"Uw totaal te betalen bedrag is $ {round(totaal , 2)}")
-
