d02b60d9267ccceb1bc3441039503116ddac277a
B078 <sbverschoor7@gmail.com>
2023-10-11 13:35:16 +0200

    opdracht 2 en feestlunch herstel

--- omgaan met gegevens/feestlunchh.py
+++ omgaan met gegevens/feestlunchh.py
@@ -1,25 +1,25 @@
 
-#prijs per stuk
+#constanten
 PRIJS_CROISSANT = 0.39
 PRIJS_STOKBROOD = 2.78
 
-#aantallen
-aantal_croissant = 17
-aantal_stokbroden = 2
+aantal_croissanten = int(input("hoeveel croissanten had u gewild? "))
+aantal_stokbroden = int(input("hoeveel stokbroden had u gewild? "))
 
-KORTINGS_BONNEN = 3
-WAARDE_BONNEN = 0.50 
+aantal_kortings_bonnen = int(input("hoeveel kortingsbonnen heeft u? "))
+waarde_korting = float(input("wat is de waarde van de bon? "))
 
 #berekening
 
-prijzen_croissant = aantal_croissant * PRIJS_CROISSANT
+prijzen_croissant = aantal_croissanten * PRIJS_CROISSANT
 prijzen_stokbroden = aantal_stokbroden * PRIJS_STOKBROOD
 
-totaal_kosten_zonder_korting = prijzen_croissant + prijzen_stokbroden
-aantal_korting = KORTINGS_BONNEN * WAARDE_BONNEN
+totaal_kosten_zonder_korting = prijzen_croissant + prijzen_stokbroden 
 
-totaal_met_korting = prijzen_croissant + prijzen_stokbroden - aantal_korting
+aantal_korting = aantal_kortings_bonnen * waarde_korting
+totaal_met_korting = totaal_kosten_zonder_korting - aantal_korting
 
 #resultaat
 print(" ")
-print(f"De feestlunch kost je bij de bakker {round(totaal_met_korting, 2 )} euro voor de {aantal_croissant} croissantjes en de {aantal_stokbroden} stokbroden als de {KORTINGS_BONNEN} kortingsbonnen nog geldig zijn!")
+print(f"Uw korting met de bonnen is ${aantal_korting}")
+print(f"De feestlunch kost je bij de bakker {round(totaal_met_korting, 2 )} euro voor de {aantal_croissanten} croissantjes en de {aantal_stokbroden} stokbroden als de {aantal_kortings_bonnen} kortingsbonnen nog geldig zijn!")
