71ee8fc63b8f18ad38276d43cbb617b0d35b582d
B078 <sbverschoor7@gmail.com>
2023-10-12 21:33:06 +0200

    ouptut

--- omgaan met conditions en forks/meten_is_weten.py
+++ omgaan met conditions en forks/meten_is_weten.py
@@ -1,34 +1,35 @@
-# Vraag de gebruiker om het eerste gehele getal (a) in te voeren
+# Vraag de gebruiker om twee gehele getallen in te voeren
 while True:
     try:
         a = int(input("Voer het eerste gehele getal (a) in: "))
         break
     except ValueError:
-        print("Ongeldige invoer voor a. Voer een geheel getal in alstublieft!")
-
-# Vraag de gebruiker om het tweede gehele getal (b) in te voeren
+        print("invoer ongeldig, voer een geheel getal in alstublieft!")
+        
 while True:
     try:
         b = int(input("Voer het tweede gehele getal (b) in: "))
         break
     except ValueError:
-        print("Ongeldige invoer voor b. Voer een geheel getal in alstublieft!")
+        print("invoer ongeldig, voer een geheel getal in alstublieft!")
 
-# Variabelen voor minimum en maximum
+#varabile
 MIN = a
 MAX = a
 
 # Controleer of a groter is dan b
 if a > b:
-    MAX = a
-    print("a is het grootste getal:", MAX)
+    Max = a
+    print("a is het grootste getal:", Max)
 # Controleer of a kleiner is dan b
 elif a < b:
-    MIN = a
-    print("a is het kleinste getal:", MIN)
-# Controleer of a en b gelijk zijn
+    Min = a
+    print("a is het kleinste getal:", Min)
+#controleer of a gelijk is aan b
 else:
     print("a en b zijn even groot.")
 
-print("Het minimum is:", MIN)
-print("Het maximum is:", MAX)
+print("het minimum is:", MIN)
+print("het maximium is", MAX)
+    
+
