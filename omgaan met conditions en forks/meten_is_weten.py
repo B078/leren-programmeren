c543bcbd56ff0526e0457ac97c8e18dc7367d0c2
B078 <sbverschoor7@gmail.com>
2023-10-12 21:15:19 +0200

    elif.statement

---
+++ omgaan met conditions en forks/meten_is_weten.py
@@ -0,0 +1,16 @@
+# Vraag de gebruiker om twee gehele getallen in te voeren
+a = int(input("Voer het eerste gehele getal (a) in: "))
+b = int(input("Voer het tweede gehele getal (b) in: "))
+
+# Controleer of a groter is dan b
+if a > b:
+    Max = a
+    print("a is het grootste getal:", Max)
+# Controleer of a kleiner is dan b
+elif a < b:
+    Min = a
+    print("a is het kleinste getal:", Min)
+else:
+    print("a en b zijn gelijk.")
+
+# Sluit het programma af
