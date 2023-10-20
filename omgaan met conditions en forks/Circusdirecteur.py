376ab3fceff1d9decd7c9f955cb7451db8b61ff9
B078 <sbverschoor7@gmail.com>
2023-10-20 20:28:49 +0200

    Update Circusdirecteur.py

--- omgaan met conditions en forks/Circusdirecteur.py
+++ omgaan met conditions en forks/Circusdirecteur.py
@@ -7,3 +7,10 @@ print("U begint nu aan uw sollicitatie voor Circusdirecteur voor Circus HotelDeB
 print("hierbij komen een aantal vragen")
 print("probeert u deze zo eerlijk mogelijk te beantwoorden")
 print("- - - - - - - - - -  - -  - - - - - - - - - - - - ")
+
+#gegevens opvagen
+input("Wat is uw naam? ")
+input("Bent u in bezit van een vrachtwagen rijbewijs? ")
+input("Bent u in bezit van een hoge hoed? ")
+input("Wat is uw lichaamsgewicht? ")
+ 
