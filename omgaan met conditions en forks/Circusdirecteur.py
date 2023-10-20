b36705ddb0bd93f1804e3f5b370a785771293e8c
B078 <sbverschoor7@gmail.com>
2023-10-20 19:48:31 +0200

    opdracht sollicicatie

---
+++ omgaan met conditions en forks/Circusdirecteur.py
@@ -0,0 +1,9 @@
+#vacature Circusdirecteur voor Circus HotelDeBotel
+
+print("- - - - - - - - - - - - - - - - - - - - - - - - - ")
+print("        sollicatieformulier 'Circusdirecteur' ")
+print("- - - - - - - - - - - - - - - - - - - - - - - - - ")
+print("U begint nu aan uw sollicitatie voor Circusdirecteur voor Circus HotelDeBotel")
+print("hierbij komen een aantal vragen")
+print("probeert u deze zo eerlijk mogelijk te beantwoorden")
+print("- - - - - - - - - -  - -  - - - - - - - - - - - - ")
