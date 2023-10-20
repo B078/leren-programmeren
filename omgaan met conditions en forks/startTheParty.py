5095c792db1818a78e017842a42b9f09ca810f91
B078 <sbverschoor7@gmail.com>
2023-10-20 21:55:23 +0200

    Update startTheParty.py

--- omgaan met conditions en forks/startTheParty.py
+++ omgaan met conditions en forks/startTheParty.py
@@ -3,7 +3,7 @@ gasten = int(input("Hoeveel gasten zijn aanwezig? "))
 drank = True  # Je kunt de waarde van drank naar True of False aanpassen, afhankelijk van de aanwezigheid van drank
 chips = False  # Je kunt de waarde van chips naar True of False aanpassen, afhankelijk van de aanwezigheid van chips
 
-if ((gastheer == "Bjorn" or gastheer == "Stemmers") and gasten >= 0 and gasten <= 20 and (gasten >= 4 or (gasten == 0 and (chips and drank)))):
+if ((gastheer == "Bjorn" or gastheer == "Slemmers") and gasten >= 0 and gasten <= 20 and (gasten >= 4 or (gasten == 0 and (chips and drank)))):
     print('Start the Party')
 else:
     print('No Party')
