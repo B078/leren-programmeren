4e7df051b2ff238ee989b1002825a8c20595076e
B078 <sbverschoor7@gmail.com>
2023-10-22 23:45:01 +0200

    terug naar begin

--- omgaan_met_conditions_en_forks/Circusdirecteur eliftest.py
+++ omgaan_met_conditions_en_forks/Circusdirecteur eliftest.py
@@ -63,8 +63,7 @@ if geslacht == "man":
                         if ervaring_dieren_dressuur >= 4 or ervaring_jongleren >= 5 or ervaring_acrobatiek >= 3:
                             if bezit_diploma.lower() == "j" or jaren_ondernemer >= 3 and aantal_werknemer_loondienst >= 5:
                                 if snor_breedte >= 10:
-                                    print(f"Gefeliciteerd {naam}, u komt in aanmerking voor de functie.")
-
+                                    print(f"Gefeliciteerd {naam}, u komt in aanmerking voor de functie.")                                 
 
 if geslacht == "vrouw":
     if rijbewijs.lower() == "j":
@@ -77,6 +76,7 @@ if geslacht == "vrouw":
                                 if soort_haar == "rood krulhaar":
                                     if lengte_haar >= 20:
                                         print(f"Gefeliciteerd {naam}, u komt in aanmerking voor de functie.")
+
 if geslacht == "anders":
     if rijbewijs.lower() == "j":
         if hoge_hoed.lower() == "j":
@@ -87,94 +87,3 @@ if geslacht == "anders":
                             if bezit_diploma.lower() == "j" or jaren_ondernemer >= 3 and aantal_werknemer_loondienst >= 5:
                                 if breedte_glimlach >= 10:
                                     print(f"Gefeliciteerd {naam}, u komt in aanmerking voor de functie.")
-
-#ouputs bij de fouten 
-#fouten bij de mannen
-fouten_man = []
-
-if geslacht == "man":
-    if not rijbewijs.lower() == "j":
-        fouten_man.append("Geen rijbewijs")
-        if not hoge_hoed.lower() == "j":
-            fouten_man.append("Geen hoge hoed")
-            if not lichaamsgewicht >= MIN_WEIGHT and lichaamsgewicht <= MAX_WEIGHT:
-                fouten_man.append("Ongeldige lichaamsgewicht (Min 90kg / Max 120kg)")
-                if not lichaamslengte >= MIN_LENGTE and lichaamslengte <= MAX_LENGTE:
-                    fouten_man.append("Ongeldige lichaamslengte (Min 150cm / Max 250cm)")
-                    if not certificaat.lower() == "j":
-                        fouten_man.append("Geen certificaat")
-                        if not ervaring_dieren_dressuur >= 4 or ervaring_jongleren >= 5 or ervaring_acrobatiek >= 3:
-                            fouten_man.append("Niet voldoende  ervaring")
-                            if not bezit_diploma.lower() == "j" or jaren_ondernemer >= 3 and aantal_werknemer_loondienst >= 5:
-                                fouten_man.append("Ongeldige diploma of niet voldoende werkservaring")
-                                if not snor_breedte >= 10:
-                                    fouten_man.append("Te smalle snor")
-if not fouten_man:
-    print("")
-
-else:
-    print("Sorry, u voldoet niet aan de volgende voorwaarden:")
-    for fout in fouten_man:
-        print(fout)                                  
-
-#fouten bij de vrouwen
-fouten_vrouw = []
-
-if geslacht == "vrouw":
-    if not rijbewijs.lower() == "j":
-        fouten_vrouw.append("Geen rijbewijs")
-        if not hoge_hoed.lower() == "j":
-            fouten_vrouw.append("Geen hoge houd")
-            if not lichaamsgewicht >= MIN_WEIGHT and lichaamsgewicht <= MAX_WEIGHT:
-                fouten_vrouw.append("Ongeldige lichaamsgewicht (Min 90kg / Max 120kg")
-                if not lichaamslengte >= MIN_LENGTE and lichaamslengte <= MAX_LENGTE:
-                    fouten_vrouw.append("Ongeldige lichaamslengte (Min 150cm / Max 250cm)")
-                    if not certificaat.lower() == "j":
-                        fouten_vrouw.append("Geen certificaat")
-                        if not ervaring_dieren_dressuur >= 4 or ervaring_jongleren >= 5 or ervaring_acrobatiek >= 3:
-                            fouten_vrouw.append("Niet voldoende  ervaring")
-                            if not bezit_diploma.lower() == "j" or jaren_ondernemer >= 3 and aantal_werknemer_loondienst >= 5:
-                                fouten_vrouw.append("Ongeldige diploma of niet voldoende werkservaring")
-                                if not soort_haar == "rood krulhaar":
-                                    fouten_vrouw.append("niet de vereiste soort haar")
-                                    if not lengte_haar >= 20:
-                                        fouten_vrouw.append("Te korte haar lengte")
-if not fouten_vrouw:
-    print("")
-
-else:
-    print("Sorry, u voldoet niet aan de volgende voorwaarden:")
-    for fout in fouten_vrouw:
-        print(fout)                                  
-
-#fouten bij anders
-fouten_anders = []
-
-
-if  geslacht == "anders":
-    if not rijbewijs.lower() == "j":
-        fouten_anders.append("Geen rijbewijs")     
-        if not hoge_hoed.lower() == "j":
-            fouten_anders.append("Geen hoge hoed")
-            if not lichaamsgewicht >= MIN_WEIGHT and lichaamsgewicht <= MAX_WEIGHT:
-                fouten_anders.append("Ongeldige lichaamsgewicht (Min 90kg / Max 120kg")
-                if not lichaamslengte >= MIN_LENGTE and lichaamslengte <= MAX_LENGTE:
-                    fouten_anders.append("Ongeldige lichaamslengte (Min 150cm / Max 250cm)")
-                    if not certificaat.lower() == "j":
-                        fouten_anders.append("Geem certificaat")
-                        if not ervaring_dieren_dressuur >= 4 or ervaring_jongleren >= 5 or ervaring_acrobatiek >= 3:
-                            fouten_anders.append("Niet voldoende  ervaring")
-                            if not bezit_diploma.lower() == "j" or jaren_ondernemer >= 3 and aantal_werknemer_loondienst >= 5:
-                                fouten_anders.append("Ongeldige diploma of niet voldoende werkservaring")
-                                if not breedte_glimlach >= 10:
-                                    fouten_anders.append("Te smalle glimlach")
-if not fouten_anders:
-    print("")
-
-else:
-    print("Sorry, u voldoet niet aan de volgende voorwaarden:")
-    for fout in fouten_anders:
-        print(fout)      
-                                 
-
-
