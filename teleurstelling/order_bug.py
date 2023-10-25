a422b616be0e9043d11129efa86ca52353a4ebe1
B078 <sbverschoor7@gmail.com>
2023-10-25 15:59:55 +0200

    Update order_bug.py

--- teleurstelling/order_bug.py
+++ teleurstelling/order_bug.py
@@ -1,18 +1,18 @@
-def vraag_getal(aantal):
-    antwoord = input("Voer het "+antwoord+" getal in: ")
-    getal = int(aantal)
+def vraag_getal(antwoord):
+    getal = input("Voer het "+antwoord+" getal in: ")
+    getal = int(getal)
     return getal
 
 def deel_getallen(a, b):
     antwoord = print(a / b)
     return antwoord
 
+getal_1 = vraag_getal("eerste")
+getal_2 = vraag_getal("tweede")
 
 if getal_2 == 0:
     print("Kan niet delen door 0")
 else:
-    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
     resultaat = deel_getallen(getal_1, getal_2)
+    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
 
-getal_2 = vraag_getal("tweede")
-getal_1 = vraag_getal("eerste")
