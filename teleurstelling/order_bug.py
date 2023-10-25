354d5aa31986e71627301128538db042793dc097
B078 <sbverschoor7@gmail.com>
2023-10-25 15:49:41 +0200

    g

---
+++ teleurstelling/order_bug.py
@@ -0,0 +1,18 @@
+def vraag_getal(aantal):
+    antwoord = input("Voer het "+antwoord+" getal in: ")
+    getal = int(aantal)
+    return getal
+
+def deel_getallen(a, b):
+    antwoord = print(a / b)
+    return antwoord
+
+
+if getal_2 == 0:
+    print("Kan niet delen door 0")
+else:
+    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
+    resultaat = deel_getallen(getal_1, getal_2)
+
+getal_2 = vraag_getal("tweede")
+getal_1 = vraag_getal("eerste")
