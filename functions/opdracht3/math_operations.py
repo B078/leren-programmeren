0c7822d3114a8cd8e459a4cbe045f9b54404965c
B078 <sbverschoor7@gmail.com>
2023-10-27 22:02:29 +0200

    g

---
+++ functions/opdracht3/math_operations.py
@@ -0,0 +1,24 @@
+# example:
+def increment(nr2: float) -> float:
+  return nr2 + 1
+
+def decrement(nr2: float) -> float:
+  return nr2 - 1
+  
+def add(nr1: float, nr2: float) -> float:
+  return nr1 + nr2 
+
+def substract(nr1: float, nr2: float) -> float:
+  return nr1 - nr2
+
+def multiply(nr1: float, nr2: float) -> float:
+  return nr1 * nr2
+
+def divide(nr1: float, nr2: float) -> float:
+  try:
+    result = nr1 / nr2
+    return result
+  except ZeroDivisionError:
+    print("kan niet dor 0 delen")
+    return None
+
