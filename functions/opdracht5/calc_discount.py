621527ee9e4df11f12585cb3cb8aaca1f3aa0c16
B078 <sbverschoor7@gmail.com>
2023-11-01 12:14:37 +0100

    g

--- functions/opdracht5/calc_discount.py
+++ functions/opdracht5/calc_discount.py
@@ -18,11 +18,11 @@ brand = input("Voer het merk in ")
 price = float(input("Wat is de prijs? "))
 
 # Bereken en toon de korting
-discount = calc_discount(price, brand, month_discount_brands)
-totaal_bedrag = price - discount
+discounts = calc_discount(price, brand, month_discount_brands)
+totaal_bedrag = price - discounts
 afgerond_totaal = round(totaal_bedrag, 2)
 
 print()
 print(f"Uw gekozen merk is: {brand}")
-print(f"Uw korting voor het merk is: {discount} euro")
+print(f"Uw korting voor het merk is: {discounts} euro")
 print(f"U totaal bedrag om te betalen is: {afgerond_totaal} euro")
