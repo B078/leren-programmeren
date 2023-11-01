004a5dfbf677565174c29f6fb863dfe3d1d7a4b0
B078 <sbverschoor7@gmail.com>
2023-11-01 11:20:09 +0100

    g

--- functions/opdracht5/AFRONDINGSCENTEN.py
+++ functions/opdracht5/AFRONDINGSCENTEN.py
@@ -3,8 +3,11 @@ from test_lib import test, report
 AFRONDINGSCENTEN = 5
 
 def rond_bedrag_af(origineel_bedrag):
-    afgerond_bedrag = round(origineel_bedrag * 20) / 20
+    afgeronde_centen = round(origineel_bedrag * 100)  # Vermenigvuldig met 100 om centen te krijgen
+    afgeronde_centen = (afgeronde_centen + AFRONDINGSCENTEN // 2) // AFRONDINGSCENTEN * AFRONDINGSCENTEN  # Rond af op centen van 0 of 5
+    afgerond_bedrag = afgeronde_centen / 100  # Terug naar bedrag met twee decimalen
     return afgerond_bedrag
+
 
 # Vraag de gebruiker om een bedrag in te voeren
 origineel_bedrag = float(input("Voer het originele bedrag in: "))
