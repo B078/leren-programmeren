79007532e1a032277e87759624bb811b84a54268
B078 <sbverschoor7@gmail.com>
2023-11-08 09:48:17 +0100

    verbetering van inzicht

--- functions/real simple/AFRONDINGSCENTEN.py
+++ functions/real simple/AFRONDINGSCENTEN.py
@@ -11,7 +11,6 @@ def rond_bedrag_af(origineel_bedrag):
 #laat een nummertje invoeren
 origineel_bedrag = float(input("Voer het originele bedrag in: "))
 afgeronde_bedrag = rond_bedrag_af(origineel_bedrag)
-expected = afgeronde_bedrag
 
 print(f"Origineel bedrag: {origineel_bedrag}")
 print(f"Afgerond bedrag: {afgeronde_bedrag}")
