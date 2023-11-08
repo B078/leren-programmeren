464a3f2a69c43fe59a0ef24c14a9445ab99a53f6
B078 <sbverschoor7@gmail.com>
2023-11-08 09:23:38 +0100

    f

--- functions/opdracht5/calculate_cilinder.py
+++ functions/opdracht5/calculate_cilinder.py
@@ -7,7 +7,7 @@ def calculate_cilinder_content(diameter, hoogte):
     inhoud = math.pi * straal**2 * hoogte
     return round(inhoud, 1)
 
-# Testgegevens
+# Testlijstje
 test_data = [
     (8.0, 5.0, 251.3),
     (11.0, 7.0, 665.2),
@@ -15,7 +15,7 @@ test_data = [
     (15.0, 2.0, 353.4),
     (0.0, 6.0, 0.0)
 ]
-
+#de nummertje bij de volgende waarde
 for diameter, hoogte, verwachte_inhoud in test_data:
     berekende_inhoud = calculate_cilinder_content(diameter, hoogte)
     test(f'Diameter {diameter}, Hoogte {hoogte}', verwachte_inhoud, berekende_inhoud)
