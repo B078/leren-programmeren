fe417acc07b25ed651f65b765c80f25d8c067262
B078 <sbverschoor7@gmail.com>
2023-11-15 11:02:35 +0100

    verbeting

--- functions/fun testen/functions-testen.py
+++ functions/fun testen/functions-testen.py
@@ -8,37 +8,37 @@ test('example', expected, calculated)
 
 nr = 13
 expected = 13.0
-calculated = float(nr) # use one function to calculate expected number
+calculated = float(nr) 
 test('to-the-point',expected, calculated)
 
 nr = -45.372
 expected = 45.372
-calculated = abs(nr) # use one function to calculate expected number
+calculated = abs(nr)
 test('optimistic', expected, calculated)
 
 nr = -45.372
 expected = -45.4
-calculated = round(nr, 1) # use one function to calculate expected number
+calculated = round(nr, 1) 
 test('approximately', expected, calculated)
 
 nr = -45.372
 expected = 45.372
-calculated = abs(nr) # use one function to calculate expected number
+calculated = abs(nr) 
 test('pessimistic', expected, calculated)
 
 nr = -2.3
 expected = -3
-calculated = floor(nr) # use one function to calculate expected number
+calculated = floor(nr) 
 test('depressed', expected, calculated)
 
 nr = int(-7.25)
 expected = -7
-calculated = round(nr, 2) # use one function to calculate expected number
+calculated = round(nr, 2) 
 test('pointless', expected, calculated)
 
 nr = 15.11
 expected = 16
-calculated = ceil(nr) # use one function to calculate expected number
+calculated = ceil(nr) 
 test('sky-is-the-limit', expected, calculated)
 
 report()
