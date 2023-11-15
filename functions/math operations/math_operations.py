fc83ac9a6f77f51b72fb07bc23e9009ebea7fbb1
B078 <sbverschoor7@gmail.com>
2023-11-15 11:56:49 +0100

    g

--- functions/math operations/math_operations.py
+++ functions/math operations/math_operations.py
@@ -15,10 +15,8 @@ def multiply(nr1: float, nr2: float) -> float:
   return nr1 * nr2
 
 def divide(nr1: float, nr2: float) -> float:
-  try:
-    result = nr1 / nr2
-    return result
-  except ZeroDivisionError:
-    print("kan niet dor 0 delen")
+  if nr2 == 0:
     return None
+  else:
+    return nr1 / nr2
 
--- teleurstelling/ice_bugs.py
+++ teleurstelling/ice_bugs.py
@@ -7,6 +7,6 @@ def convertToEuroText (amount):
 
 amount_small = int(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
 amount_medium = int(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
-totalPrice = (amount_small * SMALL_PRICE) + (amount_small * MEDIUM_PRICE)
+totalPrice = (amount_small * SMALL_PRICE) + (amount_medium * MEDIUM_PRICE)
 
 print('Dat is dan {} totaal'.format(convertToEuroText(totalPrice)))
